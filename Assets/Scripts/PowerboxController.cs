6aebcbc00026e4909888816ad60dc2a827250344
hmuaz <muazsenol1@gmail.com>
2024-07-28 13:20:08 +0300

    Stage 4 bitti gibi

--- Assets/Scripts/PowerboxController.cs
+++ Assets/Scripts/PowerboxController.cs
@@ -7,22 +7,73 @@ public class PowerboxController : MonoBehaviour
 {
     public GameObject[] roomLights;
     public DirectionalLight directionalLight;
-    public float intensityMultiplier = 1.0f;
+    public float intensityMultiplierForClose = 0;
+    public float intensityMultiplierForOpen = 1.0f;
+
+    public GameObject sartelMavi;
+    public GameObject sartelKirmizi;
+    public GameObject sartelSiyah;
+    public GameObject sartelSari;
+    public bool isCombinationRight = false;
 
 
     public void CloseLights()
     {
-        UnityEngine.RenderSettings.ambientIntensity = intensityMultiplier;
+        UnityEngine.RenderSettings.ambientIntensity = intensityMultiplierForClose;
         for (int i = 0; i < roomLights.Length; i++)
         {
             roomLights[i].SetActive(false);
         }
 
-        // if (UnityEngine.RenderSettings.skybox != null)
-        // {
-        //     // Skybox materyalini değiştirmek için
-        //     Material skyboxMaterial = UnityEngine.RenderSettings.skybox;
-        //     skyboxMaterial.SetFloat("_Exposure", intensityMultiplier);
-        // }
+    }
+
+    public void OpenLights()
+    {
+        UnityEngine.RenderSettings.ambientIntensity = intensityMultiplierForOpen;
+        for (int i = 0; i < roomLights.Length; i++)
+        {
+            roomLights[i].SetActive(true);
+        }
+
+    }
+
+    public bool IsSartelDown(GameObject sartel)
+    {
+        if (sartel.transform.eulerAngles.x > -60f)
+        {
+            return true;
+        }
+        else if (sartel.transform.eulerAngles.x < -120f)
+        {
+            return false;
+        }
+
+        return false;
+    }
+
+    public void CheckRightCombination()
+    {
+        bool kirmiziSartel = IsSartelDown(sartelKirmizi);
+        bool maviSartel = IsSartelDown(sartelMavi);
+        bool sariSartel = IsSartelDown(sartelSari);
+        bool siyahSartel = IsSartelDown(sartelSiyah);
+
+        if (kirmiziSartel == true && maviSartel == false && sariSartel == false && siyahSartel == true)
+        {
+            isCombinationRight = true;
+        }
+        else
+        {
+            isCombinationRight = false;
+
+        }
+    }
+
+    public void IsCombinationRight()
+    {
+        if (isCombinationRight)
+        {
+            OpenLights();
+        }
     }
 }
